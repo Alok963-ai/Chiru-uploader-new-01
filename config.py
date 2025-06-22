54326f1d70497835c81b4571fa99768dbb384d60
Alok963-ai <alokjangde6@gmail.com>
2025-06-23 05:06:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,23 @@
 import os
 
-API_ID = os.environ.get("API_ID", "26468828")
+API_ID = os.environ.get("API_ID", "20288951")
 
-API_HASH = os.environ.get("API_HASH", "4693513c08d1ac6af15f95b116c29478")
+API_HASH = os.environ.get("API_HASH", "e8cb5fb7a475b5f5eb3b0ef0e6ca03a8")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-#OWNER = int(os.environ.get("OWNER", 7827463899))
+#OWNER = int(os.environ.get("OWNER", 7833842279))
 
-#LOG = -1002159628443,
+#LOG = -1002548493129,
 
 # UPDATE_GRP = , # bot sat group
 
 # auth_chats = []
 
 #try:
-    #ADMINS=[7827463899]
+    #ADMINS=[7833842279]
     #for x in (os.environ.get("ADMINS", "7827463899").split()):
         #ADMINS.append(int(x))
 #except ValueError:
